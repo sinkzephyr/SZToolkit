ee72af3ff3415a64237fdb7f11bb876d6348a4bd
Sink - 王俊鑫 <sinkzephyr@gmail.com>
2017-10-02 12:07:09 +0800

    date

---
+++ lib/DateHelper.h
@@ -0,0 +1,14 @@
+//
+//  DateHelper.h
+//  studio
+//
+//  Created by zaker_sink on 2017/7/27.
+//  Copyright © 2017年 zaker_sink. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DateHelper : NSObject
+
++(NSString*)toTimeText:(NSTimeInterval)timeInterval;
+@end
---
+++ lib/DateHelper.m
@@ -0,0 +1,23 @@
+//
+//  DateHelper.m
+//  studio
+//
+//  Created by zaker_sink on 2017/7/27.
+//  Copyright © 2017年 zaker_sink. All rights reserved.
+//
+
+#import "DateHelper.h"
+
+@implementation DateHelper
+
++(NSString*)toTimeText:(NSTimeInterval)timeInterval{
+    NSDate *date = [NSDate dateWithTimeIntervalSince1970:timeInterval];
+    
+    NSDateFormatter *objDateformat = [[NSDateFormatter alloc] init];
+     [objDateformat setDateFormat:@"mm:ss"];
+    [objDateformat setTimeZone:[NSTimeZone timeZoneWithName:@"GMT"]];
+    NSString * timeStr = [objDateformat stringFromDate:date];
+    return timeStr;
+}
+
+@end
